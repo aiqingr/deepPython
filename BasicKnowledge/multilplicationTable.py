9b5d1933931019ba6c53d36d3064fe3eb6087e93
Tongyang Ni <ntyaiqingr@gmail.com>
2020-07-27 19:57:11 -0400

    multiplication table with for loop

---
+++ BasicKnowledge/multilplicationTable.py
@@ -0,0 +1,6 @@
+for i in range(1, 10):
+    # print(f"{i} x 1 = {i*1}")
+    for j in range(1, i+1):
+        print(f"{j} x {i} = {i*j}", end=" ")
+
+    print()
---
+++ BasicKnowledge/userInput.py
@@ -0,0 +1,13 @@
+name = input("Name: ")
+age = input("Age: ")
+hometown = input("Hometown: ")
+
+msg = f'''
+----------------info of {name} --------------
+Name: {name}
+Age: {age}
+hometown : {hometown}
+--------------end---------------
+'''
+
+print(msg)
